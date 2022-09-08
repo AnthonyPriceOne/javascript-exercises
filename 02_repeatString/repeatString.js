c3c5b761bcd183c5c85e73366f27baf81232f605
Anthony Aspidov <anthonypriceone@gmail.com>
2022-09-08 00:45:21 -0500

    Complete second stage 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    let sumStr = '';
+    if (times > 0) {
+        for (let i = 0; i < times; i++) {
+            sumStr += str;
+        }
+    return sumStr;
+    }
+    if (times === 0) {
+        return '';
+    }
+    if (times < 0) {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
