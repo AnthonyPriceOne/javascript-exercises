c7538064d35d0db99087b3f96e80bc2cc62fc2e7
Anthony Aspidov <anthonypriceone@gmail.com>
2022-09-09 00:41:12 -0500

    Complete fourth stage 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const sprArray = args;
+    return array.filter(item => !sprArray.includes(item));
 };
 
 // Do not edit below this line
