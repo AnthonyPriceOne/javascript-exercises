c073b999116cb4512ef6a58567b31cd51d86fbbb
Anthony Aspidov <anthonypriceone@gmail.com>
2022-09-10 19:07:17 +0300

    Complete fifth stage 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    const checkNum = args.every((ele) => {
+        return Number.isInteger(ele) && ele > 0;
+    });
+    if (checkNum === true) {
+        const justNumbers = args.sort();
+        let sumNumbers = 0;
+        for (let i = justNumbers[0]; i <= justNumbers[1]; i++) {
+        sumNumbers += i;
+    }
+    return sumNumbers;
+    }
+   return "ERROR"; 
 };
 
 // Do not edit below this line
