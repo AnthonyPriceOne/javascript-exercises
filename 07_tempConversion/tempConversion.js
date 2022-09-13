dd53309fac0a602b9b7c98a2aa41a2c115f64c81
Anthony Aspidov <anthonypriceone@gmail.com>
2022-09-13 00:22:03 -0500

    Complete seventh stage 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  const result = (far - 32) * 5 / 9;
+  const maybeDot = result.toString();
+  if (maybeDot.indexOf(".") > 0) {
+      return Number(result.toFixed(1));
+  }
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  const result = (cel * 9 / 5 + 32);
+  const maybeDot = result.toString();
+  if (maybeDot.indexOf(".") > 0) {
+      return Number(result.toFixed(1));
+  }
+  return result;
 };
 
 // Do not edit below this line
